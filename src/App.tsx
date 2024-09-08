d6fa019d99d74caa9bd2738d889ab2af0ad12d5d
Prakash Gurung <prakashgrg4400@gmail.com>
2024-09-08 17:58:11 +0545

    routing done in navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import Details from "./pages/Details";
 
 function App() {
     return (
         <>
             <div >
                 <Navbar />
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Home/> } />
+                        <Route path="/movies" element={<Movies/> } />
+                        <Route path="/movieDetails/:id" element={<Details/> } />
+                        <Route path="*" element={<Navigate to={"/"} />} />
+                    </Routes>
+                </BrowserRouter>
             </div>
         </>
     );
---
+++ src/pages/Details/index.tsx
@@ -0,0 +1,8 @@
+
+function Details() {
+  return (
+    <div>Details</div>
+  )
+}
+
+export default Details
